a57af7cf2a6b3204bc75f89f36601429cedb39de
Oscar Villalta <bagglerock@Oscars-MBP.fios-router.home>
2018-02-19 04:56:24 -0500

    still broken... will fix it later

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,108 +1,226 @@
-//  Array of characters to choose from
-
-var characters = [
-  {name: "luke", hp: 100, ap: 5, cp: 10, pic: "./assets/images/luke.jpg"},
-  {name: "obi", hp: 120, ap: 8, cp: 15, pic: "./assets/images/obi.jpg",},
-  {name: "sid", hp: 150, ap: 12, cp: 18, pic: "./assets/images/sid.png"},
-  {name: "vader", hp: 180, ap: 15, cp: 20, pic: "./assets/images/vader.jpg"}
-];
-
+//  Array of characters
+var wins = 0;
+var losses = 0;
 var characters = {
-  luke : {
-    hp : 100,
-    ap : 5,
-    cp : 10,
-    pic : "./assets/images/luke.jpg"
+  luke: {
+    name: "luke",
+    hp: 100,
+    ap: 5,
+    cp: 10,
+    pic: "./assets/images/luke.jpg"
   },
-  obi : {
-    hp : 120,
-    ap : 8,
-    cp : 15,
-    pic : "./assets/images/obi.jpg"
+  obi: {
+    name: "obi",
+    hp: 120,
+    ap: 8,
+    cp: 15,
+    pic: "./assets/images/obi.jpg"
   },
-  sid : {
-    hp : 150,
-    ap : 12,
-    cp : 18,
-    pic : "./assets/images/sid.jpg"
+  sid: {
+    name: "sid",
+    hp: 150,
+    ap: 12,
+    cp: 18,
+    pic: "./assets/images/sid.png"
   },
-  vader : {
-    hp : 180,
-    ap : 15,
-    cp : 20,
-    pic : "./assets/images/vader.jpg"
+  vader: {
+    name: "vader",
+    hp: 180,
+    ap: 15,
+    cp: 20,
+    pic: "./assets/images/vader.jpg"
+  }
+};
+
+//  Functions
+
+//initialize
+function initialize() {
+  clear();
+  displayCharacters();
+  hideStartButton();
+}
+
+function makeStartButton() {
+  var startButton = $("<button>");
+  startButton.text("Start Game").attr("id", "start");
+  $(".character-select").append(startButton);
+}
+
+function hideStartButton() {
+  $("#start").hide();
+}
+
+function showStartButton() {
+  $("#start").show();
+}
+
+function clear() {
+  $(".arena").empty();
+  $(".character-select").empty();
+}
+
+function makeCharacter(element) {
+  var character = $("<div>");
+  var image = $("<img>");
+  character
+    .addClass("character float-left unselected")
+    .attr("name", characters[element].name)
+    .attr("hit-points", characters[element].hp)
+    .attr("attack-power", characters[element].ap)
+    .attr("counter-power", characters[element].cp);
+  image.attr("src", characters[element].pic);
+  character = $(character).append(image);
+  return character;
+}
+
+//display all characters in div
+function displayCharacters() {
+  Object.keys(characters).forEach(function(key) {
+    $(".character-select").append(makeCharacter(key));
+  });
+}
+
+function heroExists() {
+  var heroLength = $("#hero").length;
+  if (heroLength === 0) {
+    return false;
+  } else {
+    return true;
   }
 }
 
+function chooseHero(name) {
+  $(name).remove();
+  $(".arena").append(name);
+  $(name)
+    .attr("id", "hero")
+    .removeClass("unselected");
+}
 
-//**  Script starts here  **//
-
-//  Click the start button to set up the game and hide the start button
-$("#start").on("click", function() {
-  $("#start").hide();
-  initGame();
-});
-
-$(".character-select").on("click", "div.character", function() {
-  var heroLength = $("#hero").length;
+function opponentExists() {
   var opponentLength = $("#opponent").length;
-  var className = $(this).attr("class");
+  if (opponentLength === 0) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
-  if (!heroLength) {
-    $(this).remove();
-    $(this).attr("id", "hero");
-    $(".arena").append(this);
-  } else if (!opponentLength && !className.includes("hero")) {
-    $(this)
+function chooseOpponent(name) {
+  $(name).remove();
+  $(".arena").append(name);
+  $(name)
     .removeClass("float-left")
     .addClass("float-right")
-    .attr("id", "opponent");
-    var attackBtn = $("<button>");
-    attackBtn.attr("id", "attack").text("Attack");
-    $(".arena").append(attackBtn);
-    $(".arena").append(this);
-    $(".character-select div.character").fadeTo(1000, 0.2);
-  }
-});
+    .attr("id", "opponent")
+    .removeClass("unselected");
+}
 
-$(".arena, #attack").on("click", function() {
-  var heroName = $("#hero").attr("name");
-  var attackPower = parseInt($("#hero").attr("attack-power"));
-  var hpHero = parseInt($("#hero").attr("hit-points"));
-  var counterPower = parseInt($("#opponent").attr("counter-power"));
-  var hpOpponent = parseInt($("#opponent").attr("hit-points"));
+function removeOpponent() {
+  $("#opponent").remove();
+}
 
-  //reduce the hp of opponent by ap of hero and double hero ap
-  hpOpponent -= attackPower;
-  attackPower = attackPower + parseInt(characters.ap); //not calculating the right ap... needs the initial ap
-  $("#hero").attr("attack-power", attackPower);
-  $("#opponent").attr("hit-points", hpOpponent);
-  console.log("Attack Power: " + attackPower);
-
-  //check the hp of the opponent before firing the next shot
-
-  //reduce the hp of the hero by cp of the opponent
-  hpHero -= counterPower;
-  $("#hero").attr("hit-points", hpHero);
-  console.log("Hero's HP: " + hpHero);
-
-  //check to see of the hero's hp is 0
-});
-
-//**  Functions  **//
-function initGame() {
-  //  Go through the array and set up each of the characters and append an image to each of the new divs
-  for (var i = 0; i < characters.length; i++) {
-    var character = $("<div>");
-    var image = $("<img>");
-    character
-      .addClass("character float-left")
-      .attr("name", characters[i].name)
-      .attr("hit-points", characters[i].hp)
-      .attr("attack-power", characters[i].ap)
-      .attr("counter-power", characters[i].cp);
-    image.attr("src", characters[i].pic);
-    $(".character-select").append(character);
-    $(character).append(image);
+function allOpponentsDead() {
+  if ($(".unselected").length === 0) {
+    return true;
+  } else {
+    return false;
   }
 }
+
+function makeAttackButton() {
+  var attackButton = $("<button>");
+  attackButton.text("Attack!").attr("id", "attack");
+  $(".arena").append(attackButton);
+}
+
+function removeAttackButton() {
+  $("#attack").remove();
+}
+
+function attackOpponent() {
+  var heroName = $("#hero").attr("name");
+  var baseAttackPower = parseInt(characters[heroName].ap);
+  var attackPower = parseInt($("#hero").attr("attack-power"));
+  var hpOpponent = parseInt($("#opponent").attr("hit-points"));
+
+  hpOpponent -= attackPower;
+  attackPower = attackPower + baseAttackPower;
+  $("#hero").attr("attack-power", attackPower);
+  $("#opponent").attr("hit-points", hpOpponent);
+}
+
+function counterHero() {
+  var hpHero = parseInt($("#hero").attr("hit-points"));
+  var counterPower = parseInt($("#opponent").attr("counter-power"));
+
+  hpHero -= counterPower;
+  $("#hero").attr("hit-points", hpHero);
+}
+
+function heroIsDead() {
+  var hpHero = parseInt($("#hero").attr("hit-points"));
+  if (hpHero <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function opponentIsDead() {
+  var hpOpponent = parseInt($("#opponent").attr("hit-points"));
+  if (hpOpponent <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function showWin() {
+  wins++;
+  console.log("You Win!!!");
+}
+
+function showLose() {
+  losses++;
+  console.log("You Lose!!!");
+}
+
+//  Script
+
+makeStartButton();
+
+$(".character-select").on("click", "#start", function() {
+  initialize();
+});
+
+$(".character-select").on("click", ".character", function() {
+  if (!heroExists()) {
+    chooseHero(this);
+  } else if (heroExists() && !opponentExists()) {
+    chooseOpponent(this);
+    makeAttackButton();
+  }
+});
+
+$(".arena").on("click", "#attack", function() {
+  attackOpponent();
+  if (opponentIsDead()) {
+    removeAttackButton();
+    removeOpponent();
+    if (!allOpponentsDead()) {
+      chooseOpponent();
+    } else {
+      showWin();
+      //show the start button again
+    }
+  } else {
+    counterHero();
+  }
+  if (heroIsDead()) {
+    showLose();
+    removeAttackButton();
+  }
+});
+//because character-select is cleared, the showstartbutton doesnt work anymore
